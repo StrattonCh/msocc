eb678b4693e451aa3a185832526f4c6ef727b0e6
Richard Erickson <11819537+rerickson-usgs@users.noreply.github.com>
2020-01-27 14:27:30 -0600

    updated Arxiv link
    Linking to the PDF caused R to segfault on my Mac. I've linked to the abstract page instead.

--- R/msocc.R
+++ R/msocc.R
@@ -13,7 +13,7 @@
 #'@seealso
 #'Useful links:
 #'  * \href{https://pubs.er.usgs.gov/publication/70194000}{Dorazio and Erickson (2017) eDNA occupancy paper}
-#'  * \href{https://arxiv.org/pdf/1205.0310.pdf}{Polson et al. (2012) polya-gamma paper}
+#'  * \href{https://arxiv.org/abs/1205.0310}{Polson et al. (2012) polya-gamma paper}
 #'  * \href{https://link.springer.com/article/10.1007/s11222-013-9416-2}{Gelman et al. (2013) information criterion paper}
 #'
 #'@name msocc
